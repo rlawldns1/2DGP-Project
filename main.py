5edf9c56146c272208e1386d951534982d8b106b
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-07 23:32:31 +0900

    좌우 움직임 구현

--- main.py
+++ main.py
@@ -3,28 +3,39 @@ from pico2d import *
 running = True
 
 def handle_events():
-    global running
+    global running, dir
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+            elif event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 open_canvas()
 
 p = load_image('punch_1.png')
 
 frame = 0
-for x in range(0,800,5):
+dir = 0
+x = 400
+while running:
     clear_canvas()
     p.clip_draw(frame*128,0,128,128,x,90)
     update_canvas()
     handle_events()
-    if not running:
-        break
     frame = (frame + 1) % 5
+    x += dir * 5
     delay(0.1)
 
 close_canvas()
