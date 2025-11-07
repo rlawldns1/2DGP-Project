352a67f37fac3dadb0785834079e194feb0ad540
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-07 23:25:14 +0900

    애니메이션 출력

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ open_canvas()
 
 p = load_image('punch_1.png')
 
+frame = 0
 for x in range(0,800,5):
     clear_canvas()
-    p.draw(400,300)
+    p.clip_draw(frame*128,0,128,128,x,90)
     update_canvas()
-    delay(0.01)
+    frame = (frame + 1) % 5
+    delay(0.1)
 
 close_canvas()
