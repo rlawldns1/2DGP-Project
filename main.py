e83b7837d76f2cc318e98123d9b428c5b1f9bdec
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-07 23:22:53 +0900

    캔버스 출력

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from pico2d import *
 
+
+
+open_canvas()
+
+p = load_image('punch_1.png')
+
+for x in range(0,800,5):
+    clear_canvas()
+    p.draw(400,300)
+    update_canvas()
+    delay(0.01)
+
+close_canvas()
