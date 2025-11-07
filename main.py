895330778af2f9bfdfad446efc9e453b7dada5bf
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-07 23:28:05 +0900

    종료 기능 구현

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from pico2d import *
 
+running = True
 
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 open_canvas()
 
@@ -11,6 +21,9 @@ for x in range(0,800,5):
     clear_canvas()
     p.clip_draw(frame*128,0,128,128,x,90)
     update_canvas()
+    handle_events()
+    if not running:
+        break
     frame = (frame + 1) % 5
     delay(0.1)
 
