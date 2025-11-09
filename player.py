efd18ada59b57c9efe23d6cb77d2587a47e0cec2
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-09 23:53:18 +0900

    leftpunch class 생성

--- player.py
+++ player.py
@@ -37,6 +37,25 @@ class Walking:
         else:
             self.player.image.clip_composite_draw(self.player.frame * 128, 0, 128, 128, 0, 'h', self.player.x, self.player.y, 512, 512)
 
+class LeftPunch:
+    def __init__(self, player):
+        self.player = player
+
+    def enter(self):
+        self.player.dir = 0
+
+    def exit(self):
+        pass
+
+    def do(self):
+        self.player.frame = (self.player.frame + 1) % 5
+
+    def draw(self):
+        if self.player.face_dir == 1:
+            self.player.image.clip_draw(self.player.frame * 128, 128, 128, 128, self.player.x, self.player.y, 512, 512)
+        else:
+            self.player.image.clip_composite_draw(self.player.frame * 128, 128, 128, 128, 0, 'h', self.player.x, self.player.y, 512, 512)
+
 class StateMachine:
     def __init__(self, start_state=None):
         self.cur_state = start_state
