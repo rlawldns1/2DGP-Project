be05281e601c5b782bf1568c966c4047004bce11
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-09 23:49:05 +0900

    walking class 및 statemachine class 생성.

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from cage import Cage
 
 running = True
 
-class Cage:
-    def __init__(self):
-        self.image = load_image('cagge.jpg')
-
-    def update(self):
-        pass
-
-    def draw(self):
-        self.image.draw(width/2, height/2)
 
 def reset_world():
     global running
--- player.py
+++ player.py
@@ -1,15 +1,48 @@
 from pico2d import *
 
 class Player:
+
     def __init__(self):
         self.x = 400
         self.y = 300
         self.frame = 0
+        self.face_dir = 1
         self.image = load_image('Walking.png')
+        self.WALKING = Walking(self)
+        self.state_machine = StateMachine(self.WALKING)
 
     def update(self):
-        self.frame = (self.frame + 1) % 12
-        self.x += 5
+        self.state_machine.update()
 
     def draw(self):
-        self.image.clip_draw(self.frame * 128, 0, 128, 128, self.x, self.y, 512, 512)
+        self.state_machine.draw()
+
+class Walking:
+    def __init__(self, player):
+        self.player = player
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def do(self):
+        self.player.frame = (self.player.frame + 1) % 12
+        pass
+
+    def draw(self):
+        if self.player.face_dir == 1:
+            self.player.image.clip_draw(self.player.frame * 128, 0, 128, 128, self.player.x, self.player.y, 512, 512)
+        else:
+            self.player.image.clip_composite_draw(self.player.frame * 128, 0, 128, 128, 0, 'h', self.player.x, self.player.y, 512, 512)
+
+class StateMachine:
+    def __init__(self, start_state=None):
+        self.cur_state = start_state
+
+    def update(self):
+        self.cur_state.do()
+
+    def draw(self):
+        self.cur_state.draw()
