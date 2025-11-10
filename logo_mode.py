a9484b2fec9c173c89a4f90a7b14bd37e1a77b57
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-10 23:17:10 +0900

    logo_mode 추가 및 구현. game_framework.py 추가

---
+++ logo_mode.py
@@ -0,0 +1,29 @@
+import game_framework
+from pico2d import *
+image = None
+running = True
+logo_start_time = 0.0
+
+def init():
+   global image, running, logo_start_time
+   image = load_image('title.png')
+   running = True
+   logo_start_time = get_time()
+
+def finish():
+   global image
+   del image
+
+def update():
+   global running, logo_start_time
+   if get_time() - logo_start_time >= 2.0:
+       logo_start_time = get_time()
+       running = False
+
+def draw():
+    clear_canvas()
+    image.draw(640, 360, 1280, 720)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from pico2d import *
-import play_mode
+import logo_mode
 
 
 width = 1280
 height = 720
 open_canvas(width,height)
 
-play_mode.init()
+logo_mode.init()
 
-while play_mode.running:
-    play_mode.handle_events()
-    play_mode.update()
-    play_mode.draw()
+while logo_mode.running:
+    logo_mode.handle_events()
+    logo_mode.update()
+    logo_mode.draw()
     delay(0.1)
 
-play_mode.finish()
+logo_mode.finish()
 close_canvas()
