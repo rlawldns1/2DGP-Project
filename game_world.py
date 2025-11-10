d68d2810e4e96ab6f46314d1eececd0f41d5ac41
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-10 21:42:28 +0900

    depth 중심으로 game_world.py 수정

--- game_world.py
+++ game_world.py
@@ -1,12 +1,25 @@
-world = []
+world = [[], []]
 
-def add_object(o):
-    world.append(o)
+def add_object(o, depth=0):
+    world[depth].append(o)
+
+def add_objects(ol, depth = 0):
+    world[depth] += ol
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+    raise ValueError('Cannot delete non existing object')
 
 def update():
-    for o in world:
-        o.update()
+    for layer in world:
+        for o in layer:
+            o.update()
 
 def render():
-    for o in world:
-        o.draw()
+    for layer in world:
+        for o in layer:
+            o.draw()
