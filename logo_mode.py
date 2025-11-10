8e871392c5fe2d5f92e6fc3fa86fbf6c167b4221
Kim Jiwoon <jiwoon031202@tukorea.ac.kr>
2025-11-10 23:22:05 +0900

    framework로 logomode 구현

--- logo_mode.py
+++ logo_mode.py
@@ -15,10 +15,10 @@ def finish():
    del image
 
 def update():
-   global running, logo_start_time
+   global logo_start_time
    if get_time() - logo_start_time >= 2.0:
        logo_start_time = get_time()
-       running = False
+       game_framework.quit()
 
 def draw():
     clear_canvas()
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from pico2d import *
 import logo_mode
+import game_framework
 
 
 width = 1280
 height = 720
 open_canvas(width,height)
 
-logo_mode.init()
-
-while logo_mode.running:
-    logo_mode.handle_events()
-    logo_mode.update()
-    logo_mode.draw()
-    delay(0.1)
-
-logo_mode.finish()
+game_framework.run(logo_mode)
 close_canvas()
